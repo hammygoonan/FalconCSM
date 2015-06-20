e39b5ff718c5306df44536b685e74a3f159535b7
Hammy Goonan <hammy@spiresoftware.com.au>
2015-06-20 17:16:28 +0100

    basic post edit layout

--- falconcms/static/js/app.js
+++ falconcms/static/js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+(function($){
+
+    $(document).foundation();
+    $(document).ready(function(){
+        if($('#content').length > 0){
+            setInterval(function(){
+                var text = $('#content').val();
+                console.log(text);
+                var converter = new Markdown.Converter();
+                var html = converter.makeHtml(text);
+                $('#preview').html(html);
+            }, 1000);
+        }
+    });
+})(jQuery);
